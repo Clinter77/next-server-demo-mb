c8982a36f7af31d4afd867d37c599c3a4991b634
clinter77 <clintchris7777@laposte.net>
2024-05-28 15:52:31 +0200

    commit du 28/05/2024 à 15h52

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return '<h3 style="color:blue; text-align:center; margin:auto; margin-top:20vh;font-size:3em;">Welcome on my Web site :)</h3>'
 
 app.run(host='0.0.0.0', port=81)
